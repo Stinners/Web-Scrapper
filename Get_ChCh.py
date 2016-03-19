01f6ac9d7d4142fba37966021a7173d8ef2bfd34
Chris Stinson <chris.stinson202@gmail.com>
2016-03-19 15:23:31 +1300

    Retreiving dates for Chch

--- Get_ChCh.py
+++ Get_ChCh.py
@@ -2,8 +2,14 @@
 # getting this will require following the link to each individual
 # consultation page and extracting the text from there
 
+# Note in general returning an empty string means the receiving 
+# function will ignore the output 
+
 
 from utilities import get_html, add_elem 
+# - get_html retreives htm from an address and returns it as a soup
+# - add_elem takes a list and an element and adds the element to the 
+#    list if non empty, this is used when searches could return blank feilds
 
 chch_adress = "http://www3.ccc.govt.nz/CCC.Web.ProjectInfo/"
 
@@ -27,17 +33,29 @@ def get_link(row):
         link = ''
     return(link)
 
+def get_date(row):
+    text = row.text.strip() 
+    is_date = ("p.m" in text) or ("a.m" in text)
+    if is_date:
+        date = text 
+    else:
+        date = ''
+    return date
+
 # Main Block 
 
 def christchurch():
-    soup = get_html(chch_adress)
+    try:
+        soup = get_html(chch_adress)
+    except ValueError:
+        return("Invalid Adress: Christchurch")
     table = soup.find("table").find_all("td")
-    names = []; links = []; info = [];
+    names = []; links = []; info = []; dates = []
 
     for row in table:
         names = add_elem(names, get_name(row))
         links = add_elem(links, get_link(row))
-
-    consults = [[names[i], links[i], []] for i in range(len(names))]
+        dates = add_elem(dates, get_date(row))
+    consults = [[names[i], links[i], dates[i], []] for i in range(len(names))]
     return(consults)
 
