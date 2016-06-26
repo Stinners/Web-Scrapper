feb128e5e8719c9579f80c7974a988af77344585
Chris Stinson <chris.stinson202@gmail.com>
2016-06-26 13:52:17 +1200

    Minor fix to wellington

--- Get_Wellington.py
+++ Get_Wellington.py
@@ -1,6 +1,5 @@
-# Note Wellington has a "featured consultation" that this script does't
+# Note Wellington has a "featured consultation" that this script doesn't
 # retreive
-# will need to look at the javascript to get that
 
 from utilities import get_html, add_elem, clean
 
@@ -13,15 +12,20 @@ def wellington():
     except ValueError:
         return "Invalid Address: Wellington"
 
-    consults = soup.find("table").find_all("td")
-    names = []; dates = []
-    for i, text in enumerate(consults):
-        # retreiving the text out of the html tags and saving it
-        # each second line is the date of the preceding consultation
-        if (i % 2) == 0:
-            name = clean(text.text.strip())
-            names.append(name)
-        else:
-            dates.append(text.text.strip())
-    consults_list = [[names[i], "",  dates[i], "", []] for i in range(len(names))]
+    try:
+        consults = soup.find("table").find_all("td")
+        names = []; dates = []
+        for i, text in enumerate(consults):
+            # retreiving the text out of the html tags and saving it
+            # each second line is the date of the preceding consultation
+            if (i % 2) == 0:
+                name = clean(text.text.strip())
+                names.append(name)
+            else:
+                dates.append(text.text.strip())
+        consults_list = [[names[i], "",  dates[i], "", []] for i in range(len(names))]
+
+    # return an empty list if there are not open consultations other than the featured one
+    except:
+        consults_list = []
     return consults_list
